e16fef80417d7e5793bea0738629fbb9960848eb
logickoder <chukwudumebiorazulike@gmail.com>
2023-03-24 04:52:21 +0100

    chore: add app fonts and colors

---
+++ lib/common/configuration/app_resources.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppFonts {
+  static const defaultFont = 'TT-Commons';
+}
+
+class AppColors {
+  static const primary = Color(0xFF4168F6);
+  static const text = Color(0xFF011B33);
+}
---
+++ lib/common/configuration/app_routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static const getStarted = '/';
+
+  static Map<String, Widget Function(BuildContext)> get routes => {};
+}
